cf30eda6e08c805effda76e48ccbedf86e608be6
CandiolliMateus <mateuscandiolli.1909@gmail.com>
2023-09-13 11:48:55 -0300

    Inicializando api-cliente

---
+++ src/main/java/com/api/cliente/ApiClienteApplication.java
@@ -0,0 +1,21 @@
+package com.api.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApiClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClienteApplication.class, args);
+	}
+
+	@GetMapping("/olamundo")
+	public String olaMundo() {
+		return "ESTE É O OLÁ MUNDO DA API-CLIENTE!";
+	}
+
+}
