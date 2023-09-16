e9d32a9ff207f39fb1ed9579a31eb1a2fd6673c6
CandiolliMateus <mateuscandiolli.1909@gmail.com>
2023-09-15 21:25:08 -0300

    Refatorando ClienteModel.

--- src/main/java/com/api/cliente/models/ClienteModel.java
+++ src/main/java/com/api/cliente/models/ClienteModel.java
@@ -9,6 +9,8 @@ import java.util.UUID;
 @Table(name = "Clientes")
 public class ClienteModel implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
